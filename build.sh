da73fdb756a68652a7ab2988d7e7e66931550f6c
rexim <reximkut@gmail.com>
2016-08-14 02:26:19 +0600

    Ready. Set. Go!

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env sh
+
+set -ex
+
+patchSfml() {
+    echo "unimplemented"
+    exit 1
+}
+
+buildSfml() {
+    echo "unimplemented"
+    exit 1
+}
+
+generateSfmlDocset() {
+    echo "unimplemented"
+    exit 1
+}
+
+patchSfml
+buildSfml
+generateSfmlDocset
